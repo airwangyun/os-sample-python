c566b3bac098013ca3d40fb07576fcbcd0e76a1b
Wang Yun <cdlwangy@cn.ibm.com>
2020-09-14 17:02:26 +0800

    chang to connect to IBM i

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,8 @@ application = Flask(__name__)
 @application.route("/<string:test>/")
 def hello(test):
     http = urllib3.PoolManager()
-    request = http.request("GET", "http://{}/web/services/students".format(test))
+    # request = http.request("GET", "http://{}/web/services/students".format(test))
+    request = http.request("GET", "www.google.com")
     # print(request.data.decode())
 
     return request.data.decode()
