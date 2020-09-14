08dcffd31fc152a4787421465a9a25a088f22d19
Wang Yun <cdlwangy@cn.ibm.com>
2020-09-14 15:24:54 +0800

    chang to connect to IBM i

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import urllib2
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+
+@application.route("/<string:test>/")
+def hello(test):
+    request = urllib2.Request("http://{}/web/services/students".format(test))
+    request.add_header('content-TYPE', 'application/x-www-form-urlencoded')
+    response = urllib2.urlopen(request)
+    return response.read()
+
 
 if __name__ == "__main__":
     application.run()
