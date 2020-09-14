6a7c13190af6b1df1dcc23770f150c70d252bfbc
Wang Yun <cdlwangy@cn.ibm.com>
2020-09-14 16:49:59 +0800

    chang to connect to IBM i

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='myFlaskApp',
+      version='1.0',
+      description='myFlaskApp',
+      author='Wang Yun',
+      author_email='cdlwangy@cn.ibm.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask==1.1.2', 'urllib3==1.25.10'],
+     )
