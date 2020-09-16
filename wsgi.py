c4daad1d8e0101457ac5f37d157074edf39db037
Wang Yun <cdlwangy@cn.ibm.com>
2020-09-16 13:46:27 +0800

    chang to connect to IBM i

--- wsgi.py
+++ wsgi.py
@@ -15,8 +15,10 @@ def hello():
 
     input2 = request2.data.decode()
 
-    outputhtml = "<br /><b>Flight information application running in OpenShift Container Platform</b><br /><br />" \
+    outputhtml = "<br /><b>Flight information application running in OpenShift Container Platform " \
+                 "with data retrieved from IBM i rest service.</b><br /><br />" \
                  "<table>" \
+                 "<tr><th>From City</th><th>To City</th></tr>" \
                  "<tr><td>{}</td><td>{}</td></tr>" \
                  "</table>".format(json2html.convert(json=input), json2html.convert(json=input2))
 
